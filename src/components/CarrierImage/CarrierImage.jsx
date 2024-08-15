4a2e17a63df046a1ed9737067d7834cafbfbf529
OxY623 <fuzzydoorproductions0@gmail.com>
2024-08-15 16:06:18 +0300

    Поправки

--- src/components/CarrierImage/CarrierImage.jsx
+++ src/components/CarrierImage/CarrierImage.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
-import styles from './CarrierImage.module.scss' // Обратите внимание, что имя файла стилей нужно заменить на ваше
+import styles from './CarrierImage.module.scss'
 
 const CarrierImage = ({ carrier }) => {
-  // Формируем URL для изображения
   const imageUrl = `https://pics.avs.io/99/36/${carrier}.png`
 
   return (
