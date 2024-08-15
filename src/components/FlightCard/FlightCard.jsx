2c6b8b859d42ba223d4e87fd79c0062f97bb131e
OxY623 <fuzzydoorproductions0@gmail.com>
2024-08-15 14:58:29 +0300

    refactoring

--- src/components/FlightCard/FlightCard.jsx
+++ src/components/FlightCard/FlightCard.jsx
@@ -2,43 +2,13 @@ import React from 'react'
 
 import CarrierImage from '../CarrierImage'
 
+import {
+  formatTime,
+  formatParagraf,
+  formatTimeStr,
+  formatTitle,
+} from './format'
 import styles from './FlightCard.module.scss'
-
-function formatTimeStr(dateStr, duration) {
-  // Создаем объект Date из строки
-  const departureDate = new Date(dateStr)
-
-  // Извлекаем часы и минуты времени отправления
-  const departureHours = departureDate.getUTCHours()
-  const departureMinutes = departureDate.getUTCMinutes()
-
-  // Создаем новый объект Date для времени прибытия
-  const arrivalDate = new Date(departureDate.getTime() + duration * 60000) // Продолжительность в миллисекундах
-
-  // Извлекаем часы и минуты времени прибытия
-  const arrivalHours = arrivalDate.getUTCHours()
-  const arrivalMinutes = arrivalDate.getUTCMinutes()
-
-  // Форматируем строки времени
-  const departureTime = `${departureHours.toString().padStart(2, '0')}:${departureMinutes.toString().padStart(2, '0')}`
-  const arrivalTime = `${arrivalHours.toString().padStart(2, '0')}:${arrivalMinutes.toString().padStart(2, '0')}`
-
-  return `${departureTime} - ${arrivalTime}`
-}
-
-function formatTime(totalMinutes) {
-  const hours = Math.floor(totalMinutes / 60)
-  const minutes = totalMinutes % 60
-  return `${hours.toString().padStart(2, '0')}ч ${minutes.toString().padStart(2, '0')}м`
-}
-
-function formatTitle(number) {
-  return number === 1 ? 'пересадка' : `${number} пересадки`
-}
-
-function formatParagraf(arr) {
-  return arr.join(', ')
-}
 
 const FlightCard = ({ price = 99, carrier = 'DP', segments = [] }) => {
   let listSchedule = segments.map((segment, index) => {
